91661c5f1f262981e3f8c8e633d81f45d328ab81
Paul Osinowo <paulosinowo@Pauls-MacBook-Pro.local>
2022-09-15 22:10:12 +0100

    change staticfiles storage

--- tasktracker/settings.py
+++ tasktracker/settings.py
@@ -131,7 +131,7 @@ STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
 )
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
