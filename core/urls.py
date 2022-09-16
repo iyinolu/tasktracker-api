0bae674bc1709e1bb030e7c4d0a29a2d7d5dbd51
Paul Osinowo <paulosinowo@Pauls-MacBook-Pro.local>
2022-09-16 05:20:17 +0100

    update user endpoint

--- core/urls.py
+++ core/urls.py
@@ -7,7 +7,7 @@ from core import views
 router = DefaultRouter(trailing_slash=True)
 
 router.register(r'api/user', views.UserViewSet, basename="user-crud")
-router.register(r'api/create', views.CreateUserViewSet, basename="signin")
+router.register(r'api/create-user', views.CreateUserViewSet, basename="signin")
 
 urlpatterns = [
     path('', include(router.urls))
